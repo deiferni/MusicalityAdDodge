f1bec11cd1ab249856dea60e22000ec4ee3dd195
deiferni <dave.erni@gmail.com>
2013-11-02 00:33:43 +0100

    Add function to auto-resume playback.

--- grooveshark_ad_dodge.js
+++ grooveshark_ad_dodge.js
@@ -1,7 +1,22 @@
+function autoResume() {
+    var link = $('#lightbox-footer-right a');
+    if (link.length && link.text() === "Resume playback") {
+        link.click();
+    }
+    setTimeout(autoResume, 1000);
+}
+
+function removeAd() {
+    $('#capital-300x250').remove();
+}
+
 function adDodgeInstall() {
 	if (typeof Grooveshark != "undefined") {
-        $(document).bind("ajaxComplete", function () {$('#capital-300x250').remove()});
-        $(document).ready(function () {$('#capital-300x250').remove()});
+        $(document).bind("ajaxComplete", removeAd);
+        $(document).ready(function () {
+            removeAd();
+            setTimeout(autoResume, 20000);
+        });
 	}
 	else {
 		setTimeout(adDodgeInstall, 500);
