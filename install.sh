08328f89c1cf4f7a5cbdb149d9afd6cae2e112d0
deiferni <dave.erni@gmail.com>
2013-11-03 16:52:06 +0100

    Make (un-)installation a bit more verbose.

--- install.sh
+++ install.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
-cp /Applications/Musicality.app/Contents/Resources/MCGroovesharkService.js /Applications/Musicality.app/Contents/Resources/MCGroovesharkService.js.bak
+original=/Applications/Musicality.app/Contents/Resources/MCGroovesharkService.js
+backup=/Applications/Musicality.app/Contents/Resources/MCGroovesharkService.js.bak
+
+if [ -f $backup ]
+then
+    echo 'Restoring original file from backup...'
+    cp $backup $original
+elif [ -f $original ]
+then
+    echo 'Backing up original file...'
+    cp $original $backup
+else
+    echo 'File '$original' not found, aborting.'
+    exit
+fi
+
+echo 'Patching...'
 cat grooveshark_ad_dodge.js >> /Applications/Musicality.app/Contents/Resources/MCGroovesharkService.js
+echo 'Done.'
--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-cp /Applications/Musicality.app/Contents/Resources/MCGroovesharkService.js.bak /Applications/Musicality.app/Contents/Resources/MCGroovesharkService.js
+original=/Applications/Musicality.app/Contents/Resources/MCGroovesharkService.js
+backup=/Applications/Musicality.app/Contents/Resources/MCGroovesharkService.js.bak
+
+if [ -f $backup ]
+then
+    echo 'Restoring original file from backup...'
+    cp $backup $original
+    echo 'Removing backup...'
+    rm $backup
+    echo 'Done.'
+else
+    echo 'No installation found.'
+fi
